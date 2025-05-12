2f271b7fb64414c1e877e0b1ad1eac8c414b8e75
Jason M <jason.moposita1201@gmail.com>
2025-05-12 12:11:55 -0400

    Create a 16x16 grid of square divs

---
+++ main.js
@@ -0,0 +1,16 @@
+// Default size given by the instructions
+const SIZE = 16
+
+const container = document.querySelector('.container')
+
+for (let row = 0; row < SIZE; row++) {
+  const rowDiv = document.createElement('div')
+  rowDiv.classList.add('row-div')
+
+  for (let col = 0; col < SIZE; col++) {
+    const squareDiv = document.createElement('div')
+    squareDiv.classList.add('square-div')
+    rowDiv.appendChild(squareDiv)
+  }
+  container.appendChild(rowDiv)
+}
