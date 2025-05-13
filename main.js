df1f1ba5ac166371476004e0e8c81a86df9c2f64
Jason M <jason.moposita1201@gmail.com>
2025-05-13 12:22:17 -0400

    Implement hover effect on the grid divs

--- main.js
+++ main.js
@@ -4,13 +4,25 @@ const SIZE = 16
 const container = document.querySelector('.container')
 
 for (let row = 0; row < SIZE; row++) {
-  const rowDiv = document.createElement('div')
-  rowDiv.classList.add('row-div')
+	const rowDiv = document.createElement('div')
+	rowDiv.classList.add('row-div')
 
-  for (let col = 0; col < SIZE; col++) {
-    const squareDiv = document.createElement('div')
-    squareDiv.classList.add('square-div')
-    rowDiv.appendChild(squareDiv)
-  }
-  container.appendChild(rowDiv)
+	for (let col = 0; col < SIZE; col++) {
+		const squareDiv = document.createElement('div')
+		squareDiv.classList.add('square-div')
+		rowDiv.appendChild(squareDiv)
+	}
+	container.appendChild(rowDiv)
 }
+
+const squareDivs = document.querySelectorAll('.square-div')
+
+squareDivs.forEach(square => {
+	square.addEventListener('mouseover', () => {
+		square.style.background = 'red'
+	})
+
+  square.addEventListener('mouseout', () => {
+    square.style.background = ''
+  })
+})
