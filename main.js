8b6f067394427e3bcf7b9a0573ae471a57aded2c
Jason M <jason.moposita1201@gmail.com>
2025-05-13 13:36:04 -0400

    Create a function to generate the grid
    The function will take any given number between 2
    and 100 and generate a grid.

--- main.js
+++ main.js
@@ -1,34 +1,48 @@
-// Default size given by the instructions
-const SIZE = 16
-
 const container = document.querySelector('.container')
 
-for (let row = 0; row < SIZE; row++) {
-	const rowDiv = document.createElement('div')
-	rowDiv.classList.add('row-div')
+function generatePad(size) {
+  container.innerHTML = ''
 
-	for (let col = 0; col < SIZE; col++) {
-		const squareDiv = document.createElement('div')
-		squareDiv.classList.add('square-div')
-		rowDiv.appendChild(squareDiv)
+	for (let row = 0; row < size; row++) {
+		const rowDiv = document.createElement('div')
+		rowDiv.classList.add('row-div')
+
+		for (let col = 0; col < size; col++) {
+			const squareDiv = document.createElement('div')
+			squareDiv.classList.add('square-div')
+      squareDiv.style.width = `${800/size}px`
+      squareDiv.style.height = `${800/size}px`
+			rowDiv.appendChild(squareDiv)
+		}
+		container.appendChild(rowDiv)
 	}
-	container.appendChild(rowDiv)
-}
 
-const squareDivs = document.querySelectorAll('.square-div')
+	const squareDivs = document.querySelectorAll('.square-div')
 
-squareDivs.forEach(square => {
-	square.addEventListener('mouseover', () => {
-		square.style.background = getRandomColor()
+	squareDivs.forEach(square => {
+		square.addEventListener('mouseover', () => {
+			square.style.background = getRandomColor()
+		})
 	})
-})
+}
 
 // Helper function to generate a random hex color
 function getRandomColor() {
-  const letters = '0123456789ABCDEF'
-  let color = '#'
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)]
-  }
-  return color
+	const letters = '0123456789ABCDEF'
+	let color = '#'
+	for (let i = 0; i < 6; i++) {
+		color += letters[Math.floor(Math.random() * 16)]
+	}
+	return color
 }
+
+const btn = document.querySelector('button')
+btn.addEventListener('click', () => {
+	let size = prompt('Please enter a number from 2 - 100')
+
+	while (size < 2 || size > 100) {
+		size = prompt('Please enter a valid number from 2 - 100')
+	}
+
+	generatePad(size)
+})
